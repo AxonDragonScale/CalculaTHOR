caad06f9fcaa5a84e865686034b4551e19935c94
Ronak Harkhani <ronak.harkhani@phonepe.com>
2021-04-02 17:16:31 +0530

    Initial Commit

---
+++ CalculaTHOR/CalculaTHORApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculaTHORApp.swift
+//  CalculaTHOR
+//
+//  Created by Ronak Harkhani on 02/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculaTHORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
